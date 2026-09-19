4e50f8123f416c8cae902cffa2ef7baf1f465d11
jappaeroo <muzaffarmuhammad98@gmail.com>
2026-09-19 22:06:22 +0800

    Create Addition.java for basic addition program

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int jumlah = a + b;
+        System.out.println("Addition of " + a + " + " + b + " = " + jumlah);
+    }
+}
