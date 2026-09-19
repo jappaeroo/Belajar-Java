c2f16c3865f01cde1ecd3ef9e23e454e7cb3bf2b
jappaeroo <muzaffarmuhammad98@gmail.com>
2026-09-19 22:22:54 +0800

    Delete Addition.java

--- Addition.java
+++
@@ -1,8 +0,0 @@
-public class Addition {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int jumlah = a + b;
-        System.out.println("Addition of " + a + " + " + b + " = " + jumlah);
-    }
-}
